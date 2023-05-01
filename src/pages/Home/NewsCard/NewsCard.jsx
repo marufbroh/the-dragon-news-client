c4b97cdf7cf64199714fb3904fb1c3602f6076d3
marufbroh <marufmd775@gmail.com>
2023-05-02 01:22:11 +0600

    final commit

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -2,7 +2,8 @@ import moment from 'moment';
 import React from 'react';
 import { Card, Image } from 'react-bootstrap';
 import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from 'react-icons/fa';
-import Rating from 'react-rating';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 import { Link } from 'react-router-dom';
 
 const NewsCard = ({ news }) => {
@@ -34,11 +35,7 @@ const NewsCard = ({ news }) => {
             </Card.Body>
             <Card.Footer className="text-muted d-flex justify-content-between">
                 <div className='d-flex gap-2'>
-                    <Rating placeholderRating={rating.number}
-                        readonly
-                        emptySymbol={<FaRegStar />}
-                        placeholderSymbol={<FaStar className='text-warning' />}
-                        fullSymbol={<FaStar />} />
+                <Rating style={{ maxWidth: 100 }} value={Math.round(rating.number || 0)} readOnly />
                     <span>{rating.number}</span>
                 </div>
                 <div>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -34,7 +34,7 @@ const router = createBrowserRouter([
       {
         path: ":id",
         element: <Category />,
-        loader: ({ params }) => fetch(`http://localhost:5000/categories/${params.id}`)
+        loader: ({ params }) => fetch(`https://the-news-dragon-server-marufbroh.vercel.app/categories/${params.id}`)
       },
     ],
   },
@@ -45,7 +45,7 @@ const router = createBrowserRouter([
       {
         path: ":id",
         element: <PrivateRoute><News /></PrivateRoute>,
-        loader: ({ params }) => fetch(`http://localhost:5000/news/${params.id}`)
+        loader: ({ params }) => fetch(`https://the-news-dragon-server-marufbroh.vercel.app/news/${params.id}`)
       }
     ]
   },
